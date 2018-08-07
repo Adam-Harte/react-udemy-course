c145949d964de23b4e9583a6502831c9a9ba8a1d
Adam-harte <adamharte11@hotmail.co.uk>
2018-08-07 14:45:11 +0100

    assignment 2 completed

--- section4/assignment2/assignment-2/src/App.js
+++ section4/assignment2/assignment-2/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import Validation from './Validation';
+import Char from './Char';
 
 class App extends Component {
   state = {
@@ -11,11 +13,36 @@ class App extends Component {
     this.setState({text: newText})
   }
 
+  updateText = (chars) => {
+    const joinedText = chars.join('');
+    this.setState({text: joinedText});
+  }
+
+  removeChar = (index) => {
+    const chars = this.state.text.split('');
+    chars.splice(index, 1);
+    this.updateText(chars);
+  }
+
   render() {
+    let characters = null;
+
+    if (this.state.text.length) {
+      characters = (
+        <div>
+          { this.state.text.split('').map((char, index) => {
+            return <Char char={ char } remove={ () => this.removeChar(index) } />
+          }) }
+        </div>
+      )
+    }
+
     return (
       <div className="App">
-        <input type="text" onChange={(event) => this.textChangeHandler(event)} />
-        <p>{ this.state.text }</p>
+        <input type="text" onChange={(event) => this.textChangeHandler(event)} value={ this.state.text } />
+        <p>{ this.state.text.length }</p>
+        <Validation length={ this.state.text.length } />
+        { characters }
       </div>
     );
   }
