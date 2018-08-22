fa0ca0989f02e3c5a2a10e7a7f19de3bb748a57c
Adam-Harte <adamharte11@hotmail.co.uk>
2018-08-22 22:33:50 +0100

    section 8 progress up to lecture 124

---
+++ section8/react-section-8/src/components/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Aux from '../../hoc/Aux';
+import Classes from './Layout.css';
+
+const layout = (props) => (
+   <Aux>
+      <div>Toolbar, Sidedrawer, Backdrop</div>
+      <main className={Classes.Content}>
+         {props.children}
+      </main>
+   </Aux>
+);
+
+export default layout;
+
