8805fe46bcc15559e2d69a7ed5c4ba7fcf96b524
Adam-Harte <adamharte11@hotmail.co.uk>
2018-09-23 19:57:30 +0100

    section 8 progress up to lecture 140

--- section8/react-section-8/src/components/Layout/Layout.js
+++ section8/react-section-8/src/components/Layout/Layout.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../hoc/Aux';
+import Toolbar from '../Layout/Layout';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import classes from './Layout.css';
 
-const layout = ( props ) => (
-    <Aux>
-        <div>Toolbar, SideDrawer, Backdrop</div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
+
+    sideDrawerOpenHandler = () => {
+        this.setState({showSideDrawer: true});
+    }
+
+    toggleSideDrawerHandler = () => {
+        this.setState((prevState) => {
+            return {showSideDrawer: !prevState.showSideDrawer}
+        });
+    }
+
+    render () {
+        return (
+            <Aux>
+                <Toolbar drawerToggleClicked={this.toggleSideDrawerHandler} />
+                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerCloseHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        );
+    }
+}
 
 export default layout;
